415a8e5c668f975420558161742e5c744ad2e990
Andre <andrevictorino96@gmail.com>
2018-11-26 21:03:01 +0000

    consigo fazer um exemplo de gráfico no Category mas não no Supermarket

--- src/pages/statistics/statistics.ts
+++ src/pages/statistics/statistics.ts
@@ -13,11 +13,9 @@ import { Component} from '@angular/core';
 export class StatisticsPage {
 
   public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+    {data: [55, 48, 70, 92, 62, 55, 40, 70, 38, 47, 52, 90], label: 'By month'},
   ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   public lineChartOptions:any = {
     responsive: true
   };
@@ -49,23 +47,47 @@ export class StatisticsPage {
   ];
   public lineChartLegend:boolean = true;
   public lineChartType:string = 'line';
-  
-  public randomize():void {
-    let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-      }
-    }
-    this.lineChartData = _lineChartData;
-  }
-  
-  // Doughnut
-  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData:number[] = [350, 450, 100];
-  public doughnutChartType:string = 'doughnut';
 
+  public lineChartData2:Array<any> = [
+    {data: [40, 35, 25], label: 'Supermercados'},
+  ];
+  public lineChartLabels2:Array<any> = ['Continente', 'Pingo Doce', 'Lidl'];
+  public lineChartOptions2:any = {
+    responsive: true
+  };
+  public lineChartColors2:Array<any> = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+  public lineChartLegend2:boolean = true;
+  public lineChartType2:string = 'line';
+
+  // Doughnut
+  // public doughnutChartLabels:string[] = ['Pingo Doce', 'Continente', 'Lidl'];
+  // public doughnutChartData:number[] = [350, 450, 100];
+  // public doughnutChartType:string = 'doughnut';
 
   // events
   public chartClicked(e:any):void {
